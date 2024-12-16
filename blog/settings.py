2e3e123878c2bcc59d9caf0f372b9020da0f345f
Skokov  Roman <skokov161@gmail.com>
2024-12-16 22:07:42 +0300

    настроил static в settings + создал папку в корне и подключил к base.html стили и js файлы

--- blog/settings.py
+++ blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- blog/urls.py
+++ blog/urls.py
@@ -19,9 +19,28 @@ from django.urls import path
 from python_blog.views import main  
 from django.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+"""
+Конверторы путей Django:
+str - строки, любые символы кроме слэша '/' (по умолчанию)
+int - положительные целые числа включая 0
+slug - ASCII буквы/цифры, дефисы и подчеркивания
+uuid - уникальные идентификаторы UUID пример '075194d3-6885-417e-a8a8-6c931e272f00'
+path - строки, включая слэши '/'
+
+Пример использования:
+path('articles/<int:year>/', views.year_archive)
+path('blog/<slug:post_slug>/', views.post_detail)
+
+"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main'),
+
+    # Подключаем python_blog.urls
     path('posts/', include('python_blog.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
