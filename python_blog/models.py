859cac354987ad9d72137718749afc18343432ae
Skokov  Roman <skokov161@gmail.com>
2024-12-23 20:21:07 +0300

    таблица Post в бд

--- python_blog/models.py
+++ python_blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(null=True, blank=True, max_length=100)
