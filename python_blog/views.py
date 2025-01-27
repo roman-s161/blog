781f601cbd3effc7d851ff223dd0bb570c40980c
Skokov  Roman <skokov161@gmail.com>
2025-01-27 13:09:31 +0300

    обновил requirements, поработал с dump/load с sqlite, добавил пагинатор(css, view, templates)

--- python_blog/views.py
+++ python_blog/views.py
@@ -6,6 +6,7 @@ from .models import Post, Category, Tag
 from django.db.models import Count, Q
 from django.contrib.messages import constants as messages
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 CATEGORIES = [
     {"slug": "python", "name": "Python"},
@@ -82,10 +83,20 @@ def catalog_posts(request):
         posts = posts.order_by('-updated_at')
     else:
         posts = posts.order_by('-created_at')
+        
+    # Создаем объект пагинатора, 2 поста на страницу
+    paginator = Paginator(posts, 2)
     
+    # Получаем номер текущей страницы
+    page_number = request.GET.get('page', 1)
+    
+    # Получаем объект страницы
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'title': 'Блог',
-        'posts': posts,
+        'posts': page_obj,  # Теперь передаем страницу вместо queryset
+        'page_obj': page_obj,  # Добавляем объект страницы в контекст
     }
     
     return render(request, 'python_blog/blog.html', context)
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,26 @@
 console.log("Все подрубилось")
+console.log('Main.js loaded successfully!');
+
+function goToPage(event) {
+    event.preventDefault();
+    
+    const pageInput = document.getElementById('pageInput');
+    const pageNumber = parseInt(pageInput.value);
+    const maxPage = parseInt(pageInput.max);
+    
+    if (pageNumber && pageNumber > 0 && pageNumber <= maxPage) {
+        // Получаем форму и добавляем номер страницы
+        const form = document.getElementById('pageForm');
+        const pageField = document.createElement('input');
+        pageField.type = 'hidden';
+        pageField.name = 'page';
+        pageField.value = pageNumber;
+        form.appendChild(pageField);
+        
+        // Отправляем форму
+        form.submit();
+    } else {
+        alert(`Пожалуйста, введите число от 1 до ${maxPage}`);
+    }
+    return false;
+}
