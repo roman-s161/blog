d97cb3d1bc5af840b6ebd85c839c376ae6260520
Skokov  Roman <skokov161@gmail.com>
2025-02-03 10:53:27 +0300

    hw26: добавил в модель Post статусы для поста, сделал миграции

--- python_blog/models.py
+++ python_blog/models.py
@@ -8,6 +8,15 @@ from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
+    
+    STATUS_CHOICES = [
+        ("draft", "Черновик"),
+        ("review", "На проверке"),
+        ("reviewed", "Проверено"),
+        ("published", "Опубликовано"),
+    ]
+
+
     title = models.CharField(max_length=100, unique=True, verbose_name="Заголовок")
     slug = models.SlugField(max_length=250, unique=True, verbose_name="Слаг", blank=True, null=True)
     content = models.TextField(verbose_name="Контент")
@@ -15,6 +24,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     views = models.PositiveIntegerField(default=0, verbose_name="Просмотры")
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="review", verbose_name="Статус"
+    )
     # категория - внешний ключ
     category = models.ForeignKey(
         "Category",  # Ссылка на модель Category
